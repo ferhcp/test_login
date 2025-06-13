975607a7cba82f2289ac489434105d7114ec27f2
Bellia <fbellia@indracompany.com>
2025-06-13 18:42:28 -0300

    primer

---
+++ login.js
@@ -0,0 +1,27 @@
+function validForm() {
+    const form = document.getElementById('loginForm');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const user = form.username.value.trim();
+        const pass = form.password.value.trim();
+
+        if (user === '' || pass === '') {
+            alert('Por favor ingresa usuario y contraseña');
+            return;
+        }
+
+       
+        const validUser = 'jstest';
+        const validPass = '1234';
+
+        if (user === validUser && pass === validPass) {
+           
+            window.location.href = 'index.html';
+        } else {
+            alert('Usuario o contraseña incorrectos');
+        }
+    });
+}
+window.onload = validForm;
